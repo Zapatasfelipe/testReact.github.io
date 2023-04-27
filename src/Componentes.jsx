1bd21a5a8c933a086b2a0d7ce1e29c5bdd6e2e53
Zapatasfelipe <fi.zapatasoto@gmail.com>
2023-04-27 11:13:05 -0400

    test

---
+++ src/Componentes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Componentes = () => {
+  return (
+    <div>Componentes
+
+        <button>Google analitics</button>
+        <button>gtm</button>
+    </div>
+    
+  )
+}
